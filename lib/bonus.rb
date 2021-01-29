3b44a3a90bbdcbc794ca5da44ed88a28975551eb
Keginold Philip <keginoldp@hotmail.com>
2021-01-29 22:05:20 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
-require_relative '../lib/bonus.rb'	require_relative '../lib/bonus.rb'
+require_relative '../lib/bonus.rb'
 
 
 describe "#bonus" do 	describe "#bonus" do
