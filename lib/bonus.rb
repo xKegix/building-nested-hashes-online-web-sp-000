fdc6a2d605bd36b95528157047c6ec534ae736a1
Keginold Philip <keginoldp@hotmail.com>
2021-01-29 22:08:22 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,17 +1,6 @@
-{name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Steven", age: "30", attitude: "confused"}	         {name: "Steven", age: "30", attitude: "confused"}
-      ]	      ]
-   }, 	   },
-   :capulet => {	   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},	      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},	      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},	      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [	      :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 	          {name: "Mercutio", age: "18", attitude: "hot-headed"},
-          {name: "Nurse", age: "44", attitude: "worried"}	          {name: "Nurse", age: "44", attitude: "worried"}
-      ]	      ]
-   }	   }
-  }	  }
+pic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-	epic_tragedy[:montague][:hero][:status] = "dead"
-   epic_tragedy[:capulet][:heroine][:status] = "dea
+
+
+puts epic_tragedy  
