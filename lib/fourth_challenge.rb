e33ec579c494ec06e91e11cd5115607ed115479b
Keginold Philip <keginoldp@hotmail.com>
2021-01-29 21:57:30 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,23 +1,24 @@
-def fourth_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
-      ]
-   }, 
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
-      ]
-   }
-}
+:matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},	      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [	      :hero_friends => [
+        #your hashes here!	        {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]	      ]
+   }, 	   },
+   :capulet => {	   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},	      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},	      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},	      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [	      :heroine_friends => [
+        #your hashes here!	        {name: "Steven", age: "30", attitude: "confused"},
+            {name: "Nurse", age: "44", attitude: "worried"}
+      ]	      ]
+   }	   }
+}	}
 
-  
 
+
+
+
+end 	
 end
