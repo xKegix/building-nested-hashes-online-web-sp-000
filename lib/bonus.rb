5d525a8238fe35a8c63a8db0564c16141de11b47
Keginold Philip <keginoldp@hotmail.com>
2021-01-29 21:57:37 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -4,9 +4,9 @@ require_relative '../lib/bonus.rb'	require_relative '../lib/bonus.rb'
 
 describe "#bonus" do 	describe "#bonus" do
   # remove the 'x' from the 'xit' below to activate this test!	  # remove the 'x' from the 'xit' below to activate this test!
-	xit "uses the []= method to change the hero and heroine's status from alive to dead" do 		it "uses the []= method to change the hero and heroine's status from alive to dead" do 
+	xit "uses the []= method to change the hero and heroine's status from alive to dead" do 		it "uses the []= method to change the hero and heroine's status from alive to dead" do
     expect(bonus[:montague][:hero][:status]).to eq("dead")	    expect(bonus[:montague][:hero][:status]).to eq("dead")
     expect(bonus[:capulet][:heroine][:status]).to eq("dead")	    expect(bonus[:capulet][:heroine][:status]).to eq("dead")
-    	
+
   end	  end
 end 	end
