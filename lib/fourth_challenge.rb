8a3b9a259c9122b4e14a858df353399bb627b290
Keginold Philip <keginoldp@hotmail.com>
2021-01-29 21:58:43 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,24 +1,26 @@
-:matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},	      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [	      :hero_friends => [
-        #your hashes here!	        {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ]	      ]
-   }, 	   },
-   :capulet => {	   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},	      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},	      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},	      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [	      :heroine_friends => [
-        #your hashes here!	        {name: "Steven", age: "30", attitude: "confused"},
-            {name: "Nurse", age: "44", attitude: "worried"}
-      ]	      ]
-   }	   }
-}	}
+epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
 
+puts epic_tragedy
 
-
-
-
-end
+#  =>
+{
+   :montague => {
+      :patriarch => {name: "Michael Jordan", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+   }
+}
 end
