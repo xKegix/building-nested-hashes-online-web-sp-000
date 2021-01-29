d0591c00f728f760d41adefb854f2fe7bf3f6e49
Keginold Philip <keginoldp@hotmail.com>
2021-01-29 21:58:15 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,14 @@
 epic_tragedy = {
-   :montague => {	   :montague => {
-      :patriarch => {	      :patriarch => {
-        #your key/value pairs here	        name: "Lord Montague", age: "53"
-        },	        },
-      :matriarch => {	      :matriarch => {
-        #your key/value pairs here	        name: "Lady Montague", age: "54"
-        },	        },
-      :hero => {	      :hero => {
-        #your key/value pairs here	    name: "Romeo", age: "15", status: "alive"
-        },	        },
-      :hero_friends => []	      :hero_friends => []
-   }, 	   },
-   :capulet => {	   :capulet => {
-      :patriarch => {	      :patriarch => {
-        #your key/value pairs here	        name: "Lord Capulet", age: "50"
-        },	        },
-      :matriarch => {	      :matriarch => {
-        #your key/value pairs here	        name: "Lady Capulet", age: "51"
-        },	        },
-      :heroine => {	      :heroine => {
-        #your key/value pairs here	        name: "Juliet", age: "15", status: "alive"
-        },	        },
-      :heroine_friends => []	      :heroine_friends => []
-   }	   }
-  }	  }
-
-
-
-end 	end
+   :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => []
+   }
+}
